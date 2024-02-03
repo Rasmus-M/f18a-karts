bf526c4d24ae47372c07cc891029905da4aca8f9
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2024-02-03 17:45:20 +0100

    GPU

--- make.bat
+++ make.bat
@@ -1,2 +1,3 @@
-xas99.py -R -S -L mode7.lst -i -q -o bin/MODE7 src/mode7.a99
+xas99.py -R -S -L mode7.lst -i -q -18 -o bin/MODE7 src/mode7.a99
 java -jar tools/ea5tocart.jar bin\MODE7 "MODE 7 DEMO"
+copy .\bin\MODE78.bin .\mode7-8.bin
