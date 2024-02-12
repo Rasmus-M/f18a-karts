56a45014d4f3008aafc3e02118f9a4c71dbfce44
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2024-02-12 23:48:26 +0100

    Pretty good 3D

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("z_table:");
+        generateScaleTable2();
+    }
+
+    private static void generateScaleTable() {
+        System.out.println("scale_table:");
         double scale = 0x100;
         double horizon = 1;
         double yWorld = 8;
@@ -8,6 +13,18 @@ public class Main {
             double z = yWorld / (yScreen + horizon);
             String hex = toHex((int) Math.round(z * scale), 4);
             System.out.print((n % 8 == 0 ? "       data " : "") + hex + (n % 8 == 7 ? "\n" : ","));
+        }
+        System.out.println();
+    }
+
+    private static void generateScaleTable2() {
+        System.out.println("scale_table:");
+        double start = 0x0020;
+        double factor = 0.0;
+        for (int yScreen = 0; yScreen < 64; yScreen++) {
+            double value = start + yScreen * factor + (yScreen * yScreen) / 32.0;
+            String hex = toHex((int) Math.round(value), 4);
+            System.out.print((yScreen % 8 == 0 ? "       data " : "") + hex + (yScreen % 8 == 7 ? "\n" : ","));
         }
         System.out.println();
     }
