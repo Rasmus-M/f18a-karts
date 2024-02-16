0c298248f6ddec80fa98981fde421c0f8c2a21bc
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2024-02-16 17:25:33 +0100

    Yes another scale function

--- java/src/Main.java
+++ java/src/Main.java
@@ -17,7 +17,7 @@ public class Main {
         StringBuilder sums = new StringBuilder();
         double sum = 0;
         for (int yScreen = 0; yScreen < 64; yScreen++) {
-            double value = scaleFunc1(yScreen);
+            double value = scaleFunc4(yScreen);
             String hex = toHex((int) Math.round(value), 4);
             sb.append(yScreen % 8 == 0 ? "       data " : "").append(hex).append(yScreen % 8 == 7 ? "\n" : ",");
             double oldSum = sum;
@@ -42,15 +42,20 @@ public class Main {
 
     private static double scaleFunc2(int yScreen) {
         double x = 64 - yScreen;
-        return 0x0020 + 400 / (x + 8);
+        return 32.0 + 400 / (x + 8);
     }
 
     private static double scaleFunc3(int yScreen) {
         double x = yScreen + 24;
         double a = 1.0 / 32;
         double b = 0;
-        double c = 0x0020;
+        double c = 32.0;
         return a * Math.pow(0.25 * x, 3) + b * x + c;
+    }
+
+    private static double scaleFunc4(int yScreen) {
+        double x = yScreen + 66;
+        return Math.pow(1.05, x);
     }
 
     private static String toHex(int n, int len) {
