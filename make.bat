703fcc761a46357117209c5159efe69f0225af68
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2024-03-02 10:21:13 +0100

    Misc

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
 xas99.py -R -S -L mode7.lst -i -q -18 -o bin/MODE7 src/mode7.a99
-java -jar tools/ea5tocart.jar bin\MODE7 "MODE 7 DEMO"
+java -jar tools/ea5tocart.jar bin\MODE7 "MODE 7 DEMO" > make.log
 copy .\bin\MODE78.bin .\mode7-8.bin
