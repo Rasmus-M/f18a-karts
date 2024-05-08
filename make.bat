abbf072f27d8e47d6cbd5ab43653fa9a671f603e
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2024-05-08 21:58:02 +0200

    Demo 2

--- make.bat
+++ make.bat
@@ -2,7 +2,7 @@ xas99.py -b png\title-screen-top.a99 -o src\title-screen-top.bin
 xas99.py -b png\title-screen-bottom.a99 -o src\title-screen-bottom.bin
 
 xas99.py -R -S -L karts.lst -i -q -18 -o bin/KARTS src/karts.a99
-java -jar tools/ea5tocart.jar bin\KARTS "F18A KARTS DEMO" > make.log
+java -jar tools/ea5tocart.jar bin\KARTS "F18A KARTS DEMO 2" > make.log
 
 copy /b .\bin\KARTS8.bin + ^
     .\bin\empty512.bin + ^
